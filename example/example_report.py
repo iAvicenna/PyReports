f24379279cd6ce44247520511e477bfd711601f7
iAvicenna <sinatureli@gmail.com>
2020-09-15 00:29:45 +0100

    some variable name changes for consistency

--- example/example_report.py
+++ example/example_report.py
@@ -10,10 +10,11 @@ import os
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
-from PyReports import Report, Section, Text, Image, List, Link
 
 module_path = os.path.dirname(os.path.dirname(os.path.abspath( __file__ )))
 sys.path.append(module_path)
+
+import PyReports as pr
 
 
 #create some figures for the report
@@ -34,11 +35,11 @@ fig4,ax4 = plt.subplots(1,1)
 ax4.plot(x_range, x_range + 0.5, color='tab:green')
 plt.close()
 
-report = Report(contains_tex=True)
+report = pr.Report(contains_tex=True)
 report.add_report_title('PAGE TITLE')
 
 #create a section and add some stuff
-section1 = Section(1)
+section1 = pr.ReportSection(1)
 section1.add_section_title('Section 1 (demonstration of adding basic objects)')
 
 #add some text
@@ -52,34 +53,36 @@ section1.add_images([fig1,fig2, fig3, fig4],scales=[1]*4, captions=captions)
 section1.add_link('https://www.lipsum.com/', 'Lipsum generator')
 
 #create a subsection 1.1 and add text and two lists
-ssection = Section(1.1, is_subsection=True)
+ssection = pr.ReportSection(1.1, is_subsection=True)
 ssection.add_section_title('Sub Section 2 Title')
 ssection.add_text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fermentum posuere vestibulum. Quisque nec ante varius, efficitur velit ac, aliquet metus. Duis id magna facilisis, fringilla nisl a, faucibus libero. Donec malesuada finibus justo et iaculis. Praesent pellentesque pulvinar iaculis. Suspendisse interdum viverra nisl eget ultricies. Aenean non odio ut quam ornare ornare. Aliquam feugiat fringilla sollicitudin. Quisque posuere magna ipsum, sed placerat nulla venenatis id. Sed non velit vitae dolor sagittis pharetra.')
 
 #add a list of texts
-texts = [Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec lacinia dolor.'),
-         Text('Morbi augue justo, euismod et ante quis, laoreet congue est. Aliquam erat volutpat.'),
-         Text('Praesent pharetra magna vel nunc scelerisque interdum.', font_size=30), Link('https://www.lipsum.com/', 'Lipsum generator')]
+texts = [pr.ReportText('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec lacinia dolor.'),
+         pr.ReportText('Morbi augue justo, euismod et ante quis, laoreet congue est. Aliquam erat volutpat.'),
+         pr.ReportText('Praesent pharetra magna vel nunc scelerisque interdum.', font_size=30), pr.ReportLink('https://www.lipsum.com/', 'Lipsum generator')]
 ssection.add_list(texts)
 
-texts = [Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
-         Text('Vestibulum auctor eleifend commodo.')
+texts = [pr.ReportText('Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+         pr.ReportText('Vestibulum auctor eleifend commodo.')
          ]
 ssection.add_list(texts, is_ordered=True)
 
 
 #create new section and add a multitab containing an image, a text and a list
-section2 = Section(2)
+section2 = pr.ReportSection(2)
 section2.add_section_title('Section 2 (demonstration of multitabs)')
-list1 = List([Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
-              Text('Nullam efficitur mattis laoreet. '),
-              Text(' Maecenas cursus ornare massa, nec eleifend arcu posuere sed.', font_size=4)])
-tab_contents = [[Image(fig1),Text('Donec venenatis libero a orci pellentesque mattis.')],
-                [list1, Text('Nunc arcu est, lacinia vitae justo eget, fringilla fermentum nibh.')]]
+list1 = pr.ReportList([
+    pr.ReportText('Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+    pr.ReportText('Nullam efficitur mattis laoreet. '),
+    pr.ReportText(' Maecenas cursus ornare massa, nec eleifend arcu posuere sed.', font_size=4)])
+
+tab_contents = [[pr.ReportImage(fig1), pr.ReportText('Donec venenatis libero a orci pellentesque mattis.')],
+                [list1, pr.ReportText('Nunc arcu est, lacinia vitae justo eget, fringilla fermentum nibh.')]]
 section2.add_multitab(0, 2, ['Tab1','Tab2'], tab_contents)
 
 #create new section and add some tex formulas
-section3 = Section(3)
+section3 = pr.ReportSection(3)
 section3.add_section_title('Section 3 (demonstration of tex)')
 section3.add_text('This is proved by the following formula:')
 section3.add_text('{$\int_0^t f(s,z)ds = g(z,t),$}', alignment='center')
